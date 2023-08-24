bf06b2d8fb5df6dba84fa228594d71d882150069
천진아 <totter15@naver.com>
2023-08-24 23:38:57 +0900

    feat : 풍선 애니메이션 추가

--- index.js
+++ index.js
@@ -3,44 +3,55 @@ const home = document.querySelector('.home');
 
 const locateRotate = [];
 
-function createBalloon() {
-	function deleteBalloon(e) {
-		const nodes = e.target.parentElement;
-		nodes.className === 'balloon' && nodes.remove();
+function deleteBalloon(e) {
+	const nodes = e.target.parentElement;
+	nodes.className === 'balloon' && nodes.remove();
+}
+
+const getRandomSize = () => {
+	return Math.floor(Math.random() * (150 - 120) + 120);
+};
+
+const getRandomBalloon = () => {
+	return Math.floor(Math.random() * 9) + 1;
+};
+
+const getRandomRotate = () => {
+	let rotate = Math.floor(Math.random() * (50 + 50) - 50);
+
+	// 같은 위치에 풍선이 생기는 것 방지
+	while (locateRotate.includes(rotate)) {
+		if (!locateRotate.includes(rotate)) break;
+		rotate = Math.floor(Math.random() * (50 + 50) - 50);
 	}
+	locateRotate.push(rotate);
+	return rotate;
+};
 
-	const getRandomSize = () => {
-		return Math.floor(Math.random() * (180 - 150) + 150);
-	};
+const getRandomHeight = () => {
+	return Math.floor(Math.random() * (50 - 5) + 5);
+};
 
-	const getRandomBalloon = () => {
-		return Math.floor(Math.random() * 9) + 1;
-	};
-
-	const getRandomRotate = () => {
-		let rotate = Math.floor(Math.random() * (50 + 50) - 50);
-
-		// 같은 위치에 풍선이 생기는 것 방지
-		while (locateRotate.includes(rotate)) {
-			if (!locateRotate.includes(rotate)) break;
-			rotate = Math.floor(Math.random() * (50 + 50) - 50);
-		}
-		locateRotate.push(rotate);
-		return rotate;
-	};
-
-	const getRandomHeight = () => {
-		return Math.floor(Math.random() * (60 - 5) + 5);
-	};
-
+function createBalloon() {
 	const randomHeight = getRandomHeight();
+	const randomRotate = getRandomRotate();
+	const keyframe = [
+		{ transform: `rotate(${randomRotate - 1}deg) translate(-50%, 0%)` },
+		{ transform: `rotate(${randomRotate + 1}deg) translate(-50%, 0%)` },
+	];
+	const options = {
+		duration: 1500,
+		easing: 'ease-in-out',
+		direction: 'alternate',
+		iterations: Infinity,
+	};
 
 	const balloonList = document.querySelector('.balloon_list');
 	const balloon = document.createElement('button');
 	balloon.className = 'balloon';
-	balloon.style.transform = `rotate(${getRandomRotate()}deg) translate(-50%, 0%)`;
 	balloon.style.width = getRandomSize() + 'px';
-	balloon?.addEventListener('click', (e) => deleteBalloon(e));
+	balloon.addEventListener('click', (e) => deleteBalloon(e));
+	balloon.animate(keyframe, options);
 
 	const balloonImg = document.createElement('img');
 	balloonImg.src = `./imgs/balloon_${getRandomBalloon()}.png`;
